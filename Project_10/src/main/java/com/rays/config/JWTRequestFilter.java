e21f460d6dbc390ca505349bc3068e49afa9c17b
yashmita9 <yashmita9827@gmail.com>
2026-01-28 18:22:45 +0530

    Update JWTRequestFilter.java

--- Project_10/src/main/java/com/rays/config/JWTRequestFilter.java
+++ Project_10/src/main/java/com/rays/config/JWTRequestFilter.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.transaction.CannotCreateTransactionException;
+
 
 import com.rays.service.JWTUserDetailsService;
 
