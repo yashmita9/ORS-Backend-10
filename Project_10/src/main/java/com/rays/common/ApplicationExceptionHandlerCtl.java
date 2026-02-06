44d2acc75908ad0b54bb09fcbda8972879f1360a
yashmita9 <yashmita9827@gmail.com>
2026-02-06 20:48:17 +0530

    Add JDBCConnectionException handling to exceptions

--- Project_10/src/main/java/com/rays/common/ApplicationExceptionHandlerCtl.java
+++ Project_10/src/main/java/com/rays/common/ApplicationExceptionHandlerCtl.java
@@ -1,7 +1,8 @@
 package com.rays.common;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataAccessResourceFailureException;
+import org.hibernate.exception.JDBCConnectionException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.CannotCreateTransactionException;
@@ -11,31 +12,39 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * Handles application propagated exceptions
  * 
- * SANAT KUMAR CHOUHAN
+ * Sushobhit Pandey 
  *
  */
 
-@RestControllerAdvice // handle exception globally
+
+@RestControllerAdvice
 public class ApplicationExceptionHandlerCtl {
 
-	// All database related problems
-	@ExceptionHandler({ CannotCreateTransactionException.class, DataAccessResourceFailureException.class })
-	public ResponseEntity<ORSResponse> handleDatabaseException(Exception e) {
+    // All database related problems
+    @ExceptionHandler({
+        CannotCreateTransactionException.class,
+        DataAccessResourceFailureException.class,
+        JDBCConnectionException.class
+    })
+    public ResponseEntity<ORSResponse> handleDatabaseException(Exception e) {
 
-		ORSResponse res = new ORSResponse(false);
-		res.addMessage("Database service is currently unavailable. Please try again later.");
+        ORSResponse res = new ORSResponse(false);
+        res.addMessage("Database service is currently unavailable. Please try again later.");
 
-		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(res);
-	}
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)   // 503
+                .body(res);
+    }
 
-	// All other runtime exceptions
-	@ExceptionHandler(RuntimeException.class)
-	public ResponseEntity<ORSResponse> handleRuntimeException(RuntimeException e) {
+    // All other runtime exceptions
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ORSResponse> handleRuntimeException(RuntimeException e) {
 
-		ORSResponse res = new ORSResponse(false);
-		res.addMessage(e.getMessage());
+        ORSResponse res = new ORSResponse(false);
+        res.addMessage(e.getMessage());
 
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
-	}
-
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR) // 500
+                .body(res);
+    }
 }
